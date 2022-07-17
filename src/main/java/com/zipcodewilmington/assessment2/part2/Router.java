f7f5c8e4d52ba20b8f65e65fa863159563ed6e9a
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-17 14:49:19 -0400

    78 completed

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,20 +1,36 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.LinkedHashMap;
+
 public class Router {
+    LinkedHashMap<String, String> address;
+    LinkedHashMap<String, String> duplicate;
+    public Router(){
+        address = new LinkedHashMap<>();
+        duplicate = new LinkedHashMap<>();
+    }
     public void add(String path, String controller) {
+        if (address.containsKey(path))
+            duplicate.put(path, controller);
+        else
+            address.put(path, controller);
     }
 
     public Integer size() {
-        return null;
+//        return null;
+        return address.size();
     }
 
     public String getController(String path) {
-        return null;
+//        return null;
+        return address.get(path);
     }
 
     public void update(String path, String studentController) {
+        address.put(path, studentController);
     }
 
     public void remove(String path) {
+        address.remove(path);
     }
 }
